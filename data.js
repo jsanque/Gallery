512e2efa5f04962cc6de08927b37efa89b76bd2d
jsanque <jsanque23@gmail.com>
2025-11-25 10:01:23 +0800

    added albums gallery

---
+++ data.js
@@ -0,0 +1,34 @@
+var galleryItems = [
+    {
+        title: "Take Care (2011)",
+        image: "imgs/takeCare.jpg",
+    },
+    {
+        title: "Nothing Was the Same (2013)",
+        image: "imgs/nwts.jpg",
+    },
+    {
+        title: "Views (2016)",
+        image: "imgs/viewjpg.jpg",
+    },
+    {
+        title: "Scorpion (2018)",
+        image: "imgs/scorpion.jpg",
+    },
+    {
+        title: "Certified Lover Boy (2021)",
+        image: "imgs/clb.jpg",
+    },
+    {
+        title: "Thank Me Later (2010)",
+        image: "imgs/thank me later.jpg",
+    },
+    {
+        title: "For All The Dogs (2023)",
+        image: "imgs/faltd.jpg",
+    },
+    {
+        title: "Honestly, Nevermind (2022)",
+        image: "imgs/honest;y.jpg",
+    }
+];
